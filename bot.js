ac867b367d2c2c48ea2164d34b89f8edfcefcd44
saberb959 <48495163+saberb959@users.noreply.github.com>
2019-04-26 23:50:20 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '-';
+const adminprefix = '-';
 console.log("Bot is on By saber");
 
 client.on('message' , message => {
@@ -14,20 +15,39 @@ client.on('message' , message => {
     }  
    });
 
-   client.on("message", message => {
+//bc
 
-    if (message.content.startsWith(prefix + "bc")) {
-                 if (-message.member.hasPermission("ADMINISTRATOR"))  return;
+client.on("message", message => {
+  if (message.content.startsWith("$obc")) {
+               if (!message.member.hasPermission("ADMINISTRATOR"))  return;
 let args = message.content.split(" ").slice(1);
-var argresult = args.join(' '); 
-message.guild.members.filter(m => m.presence.status -= 'offline').forEach(m => {
+var argresult = args.join(' ');
+message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
 m.send(`${argresult}\n ${m}`);
 })
-message.channel.send(`\`${message.guild.members.filter(m => m.presence.status -= 'online').size}\` : عدد الاعضاء المستلمين`); 
-message.delete(); 
-};     
+message.channel.send(`\`${message.guild.members.filter( m => m.presence.status !== 'all').size}\`:mailbox:  عدد المستلمين `);
+message.delete();
+};
 });
 
+
+//bc online
+
+
+
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` :mailbox:  عدد المستلمين `); 
+ message.delete(); 
+};     
+});
 
 client.on('ready', () => {
 console.log(`----------------`);
@@ -41,83 +61,81 @@ client.user.setStatus("dnd")
 });
 
 client.on("message", (message) => {
-    if (-message.content.startsWith(prefix) || message.author.bot) return;
-  
-    if (message.content.toLowerCase().startsWith(prefix + `tickethelp`)) {
-      const embed = new Discord.RichEmbed()
-      .setTitle(`:mailbox_with_mail: Tuve server teckithelp`)
-      .setColor(0xCF40FA)
-      .setDescription(`Hello- I'm Tuve bot, the Discord bot for super cool support ticket stuff and more- Here are my commands:`)
-      .addField(`Tickets`, `[-new]() > Opens up a new ticket and tags the Support Team\n[-close]() > Closes a ticket that has been resolved or been opened by accident`)
-      .addField(`Other`, `[-teckithelp]() > Shows you this tickethelp menu your reading\n[-ping]() > Pings the bot to see how long it takes to react\n[-about]() > Tells you all about Phantom`)
-      message.channel.send({ embed: embed });
-    }
-  
-    if (message.content.toLowerCase().startsWith(prefix + `ping`)) {
-      message.channel.send(`Hoold on-`).then(m => {
-      m.edit(`:ping_pong: Wew, made it over the ~waves~ - **Pong-**\nMessage edit time is ` + (m.createdTimestamp - message.createdTimestamp) + `ms, Discord API heartbeat is ` + Math.round(client.ping) + `ms.`);
-      });
-  }
-  
-  if (message.content.toLowerCase().startsWith(prefix + `new`)) {
-      const reason = message.content.split(" ").slice(1).join(" ");
-      if (-message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
-      if (message.guild.channels.exists("name", "ticket-" + message.author.username)) return message.channel.send(`You already have a ticket open.`);
-      message.guild.createChannel(`ticket-${message.author.username}`, "text").then(c => {
-          let role = message.guild.roles.find("name", "Support Team");
-          let role2 = message.guild.roles.find("name", "@everyone");
-          c.overwritePermissions(role, {
-              SEND_MESSAGES: true,
-              READ_MESSAGES: true
-          });
-          c.overwritePermissions(role2, {
-              SEND_MESSAGES: false,
-              READ_MESSAGES: false
-          });
-          c.overwritePermissions(message.author, {
-              SEND_MESSAGES: true,
-              READ_MESSAGES: true
-          });
-          message.channel.send(`:white_check_mark: Your ticket has been created, #${c.name}.`);
-          const embed = new Discord.RichEmbed()
-          .setColor(0xCF40FA)
-          .addField(`Hey ${message.author.username}-`, ` *** Support Team *** يرجى محاولة شرح سبب فتح هذه التذكرة بأكبر قدر ممكن من التفاصيل. سيكون فريق الدعم  هنا قريباً لمساعدتك :)   -- لغلق التذكره اكتب .close.`)
-          .setTimestamp();
-          c.send({ embed: embed });
-      }).catch(console.error);
-  }
-  if (message.content.toLowerCase().startsWith(prefix + `close`)) {
-      if (-message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
-  
-      message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action-\nTo confirm, type \`-confirm\`. This will time out in 10 seconds and be cancelled.`)
-      .then((m) => {
-        message.channel.awaitMessages(response => response.content === '-confirm', {
-          max: 1,
-          time: 10000,
-          errors: ['time'],
-        })
-        .then((collected) => {
-            message.channel.delete();
-          })
-          .catch(() => {
-            m.edit('Ticket close timed out, the ticket was not closed.').then(m2 => {
-                m2.delete();
-            }, 3000);
-          });
-      });
-  }
-  
-  });
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  client.on('message',async message => {
+  if (message.content.toLowerCase().startsWith(prefix + `tickethelp`)) {
+    const embed = new Discord.RichEmbed()
+    .setTitle(`:mailbox_with_mail: Vulnix teckithelp`)
+    .setColor(0xCF40FA)
+    .setDescription(`Hello! I'm Phantom, the Discord bot for super cool support ticket stuff and more! Here are my commands:`)
+    .addField(`Tickets`, `[-new]() > Opens up a new ticket and tags the Support Team\n[-close]() > Closes a ticket that has been resolved or been opened by accident`)
+    .addField(`Other`, `[-teckithelp]() > Shows you this tickethelp menu your reading\n[-ping]() > Pings the bot to see how long it takes to react\n[-about]() > Tells you all about Phantom`)
+    message.channel.send({ embed: embed });
+  }
+
+  if (message.content.toLowerCase().startsWith(prefix + `ping`)) {
+    message.channel.send(`Hoold on!`).then(m => {
+    m.edit(`:ping_pong: Wew, made it over the ~waves~ ! **Pong!**\nMessage edit time is ` + (m.createdTimestamp - message.createdTimestamp) + `ms, Discord API heartbeat is ` + Math.round(client.ping) + `ms.`);
+    });
+}
+
+if (message.content.toLowerCase().startsWith(prefix + `new`)) {
+    const reason = message.content.split(" ").slice(1).join(" ");
+    if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
+    if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`You already have a ticket open.`);
+    message.guild.createChannel(`ticket-${message.author.id}`, "text").then(c => {
+        let role = message.guild.roles.find("name", "Support Team");
+        let role2 = message.guild.roles.find("name", "@everyone");
+        c.overwritePermissions(role, {
+            SEND_MESSAGES: true,
+            READ_MESSAGES: true
+        });
+        c.overwritePermissions(role2, {
+            SEND_MESSAGES: false,
+            READ_MESSAGES: false
+        });
+        c.overwritePermissions(message.author, {
+            SEND_MESSAGES: true,
+            READ_MESSAGES: true
+        });
+        message.channel.send(`:white_check_mark: Your ticket has been created, #${c.name}.`);
+        const embed = new Discord.RichEmbed()
+        .setColor(0xCF40FA)
+        .addField(`Hey ${message.author.username}!`, ` *** Support Team *** يرجى محاولة شرح سبب فتح هذه التذكرة بأكبر قدر ممكن من التفاصيل. سيكون فريق الدعم  هنا قريباً لمساعدتك :)   -- لغلق التذكره اكتب -close.`)
+        .setTimestamp();
+        c.send({ embed: embed });
+    }).catch(console.error);
+}
+if (message.content.toLowerCase().startsWith(prefix + `close`)) {
+    if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
+
+    message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`-confirm\`. This will time out in 10 seconds and be cancelled.`)
+    .then((m) => {
+      message.channel.awaitMessages(response => response.content === '-confirm', {
+        max: 1,
+        time: 10000,
+        errors: ['time'],
+      })
+      .then((collected) => {
+          message.channel.delete();
+        })
+        .catch(() => {
+          m.edit('Ticket close timed out, the ticket was not closed.').then(m2 => {
+              m2.delete();
+          }, 3000);
+        });
+    });
+}
+
+});
+
+client.on('message',async message => {
     var room;
     var title;
     var duration;
     var gMembers;
     var filter = m => m.author.id === message.author.id;
     if(message.content.startsWith(prefix + "giveaway")) {
-  
-      if(-message.guild.member(message.author).hasPermission('MANAGE_GUILD')) return message.channel.send(':heavy_multiplication_x:| **you should have "MANAGE GUILD" permission**');
       message.channel.send(`:eight_pointed_black_star:| **in wich channel you want to start the giveaway?**`).then(msgg => {
         message.channel.awaitMessages(filter, {
           max: 1,
